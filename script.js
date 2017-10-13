63b95a28481cba35ea34d3f07009650ca894c10b
Mary Clare Harrington <maryclareharrington@Marys-MacBook-Air.local>
2017-10-13 07:06:30 -0400

    my second commit

---
+++ script.js
@@ -0,0 +1,93 @@
+// Exercise 1
+
+/*
+var q = prompt("enter a number");
+var total = 0;
+
+for (var num = 0; num <= q; num+=1) {
+    total += num;
+}
+console.log(total);
+*/
+
+
+
+
+// Exercise 2
+
+/*
+var play = prompt("do u want to play?");
+ 
+do {
+    var word = prompt("enter a word");
+    answer = prompt("do u wanna play again?")
+    
+    if (answer == "yes"); {
+        var word2 = prompt ("enter a word")
+        answer2 = prompt ("do u wanna play again?")
+    }
+    
+}while (answer2 == "yes");
+ 
+console.log(word + " " + word2);
+*/
+
+
+
+// Exercise 3
+
+/*var question = prompt("do u want to print ur name?");
+
+if (question == "yes") {
+    var name = prompt("what is ur name?");
+    console.log("Hello my name is " + name);
+}
+
+var question2 = prompt("do u want to print ur name again?");
+var x = "!";
+
+while (question2 == "yes"); {
+   console.log("Hello my name is " + name + x);
+    x++;
+} 
+*/
+
+// Exercise Four
+/*
+var time = prompt("what time of day is it?");
+ 
+if (time == "morning") {
+     console.log("Since it is morning, you should be eating breakfast. We suggest eggs and toast.");
+} else if (time == "noon") {
+    console.log("Since it is afternoon, you should be eating lunch. We suggest a salad.");
+} else if (time == "evening") {
+    console.log("Since it is evening, you should be eating dinner. We suggest chicken and rice.");
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
